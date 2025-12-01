7eb5806e845f7693e992aa4f7513fe51f0c5a688
Renato Zampa <renatomexzampa9@gmail.com>
2025-12-01 17:17:08 -0300

    feat: Criando página home e configurando o sistema de rotas no server.js
    closed #2

--- src/server.js
+++ src/server.js
@@ -5,6 +5,27 @@ const PORT = 3000
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.render("home")
+});
+app.get('/questoes', (req, res) => {
+    res.status(200).render("questoes")
+})
+app.get('/categorias', (req, res) => {
+    res.status(200).render("categorias")
+})
+app.get('/login', (req, res) => {
+    res.status(200).render("login")
+})
+
 
 
 app.listen(PORT, () => {
