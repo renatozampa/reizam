debc824c9daf2faf53bce89e0ac16daf28ab63cb
Renato Zampa <renatomexzampa9@gmail.com>
2025-12-15 20:50:24 -0300

    test: Deve encontrar questão por ID existente
    closed #25

---
+++ __tests__/questoes-crud.test.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+jest.mock('fs');
+
+const {
+    buscarQuestaoPorId,
+} = require('../src/lib/library');
+
+describe('CRUD de Questões', () => {
+    
+    describe('buscarQuestaoPorId()', () => {
+        
+        beforeEach(() => {
+            const questoesMock = [
+                { id: 1, questao: "Quanto é 2+2?", correta: "A" },
+                { id: 2, questao: "Quanto é 3+3?", correta: "B" },
+                { id: 5, questao: "Quanto é 5+5?", correta: "C" }
+            ];
+            fs.readFileSync.mockReturnValue(JSON.stringify(questoesMock));
+        });
+
+        test('Cenário 11: Deve encontrar questão por ID existente', () => {
+            const resultado = buscarQuestaoPorId(1);
+            expect(resultado).toBeDefined();
+            expect(resultado.id).toBe(1);
+        });
+    })
+        
+
+    
+});
